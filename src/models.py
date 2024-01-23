27a47243fa242876001c7ef2c1930aa8c1dc30e1
TheJeth <145587508+TheJeth@users.noreply.github.com>
2024-01-23 23:36:17 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -2,28 +2,49 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
+
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email= Column(String(20), nullable=False)
+    tel = Column(String(10), nullable=False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_planet = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable= False)
+    planet_gravity = Column(Integer, nullable= False)
+    planet_climate = Column(Integer, nullable=False)
+    planet_population = Column(Integer, nullable=False)
+    planet_terrain = Column(String(250), nullable= False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id_starship = Column(Integer, primary_key=True)
+    starship_name = Column(String(250), nullable= False)
+    starship_model = Column(String(250), nullable= False)
+    starship_length = Column(Integer, nullable=False)
+    starship_CostInCredit = Column(Integer, nullable=False)
+    starship_manufacturer = Column(String(250), nullable= False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
